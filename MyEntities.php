700bc9668ac2340aff880d4a23202b2839b1c40c
Mirco <mirco.paz@gmail.com>
2020-11-17 22:27:22 +0100

    Add: sample entitie and sample database

--- MyEntities.php
+++ MyEntities.php
@@ -1,3 +1,97 @@
 <?php
 
-$items = [];
+class User extends Entity
+{
+    public $title = "Users";
+    public $data = array(
+            "id" => [
+                "type" => DataType::KEY,
+                "label" => "Id",
+                "showInList" => false,
+                "showInDetail" => false,
+                "nullable" => false],
+
+            "username" => [
+                "type" => DataType::STRING,
+                "label" => "Username",
+                "showInList" => true,
+                "showInDetail" => true,
+                "nullable" => false],
+
+            "password" => [
+                "type" => DataType::STRING, 
+                "label" => "Password", 
+                "showInList" => false, 
+                "showInDetail" => true,
+                "nullable" => false],
+
+            "last_access" => [
+                "type" => DataType::DATETIME, 
+                "label" => "Last Access", 
+                "showInList" => true, 
+                "showInDetail" => false,
+                "nullable" => true],
+
+            "expire_date" => [
+                "type" => DataType::DATETIME, 
+                "label" => "Expires", 
+                "showInList" => true, 
+                "showInDetail" => true,
+                "nullable" => true],
+
+            "privilege_id" => [
+                "type" => DataType::FK, 
+                "label" => "Privilege", 
+                "showInList" => true, 
+                "showInDetail" => true,
+                "nullable" => false],
+
+            "enabled" => [
+                "type" => DataType::BOOL, 
+                "label" => "Is Ebabled", 
+                "showInList" => true, 
+                "showInDetail" => true,
+                "nullable" => false],
+        );
+    public $fk = array();
+    public $table = "user";
+
+    function __construct()
+    {
+        $this->fk = array(
+            0 => new ForeignKey(
+                "fk_privilege", 
+                "Corso", 
+                array("privilege_id"), 
+                array("id"), 
+                new Privilege(), 
+                "name"),
+        );
+    }
+}
+
+class Privilege extends Entity
+{
+    public $title = "Privileges";
+    public $data = array(
+            "id" => [
+                "type" => DataType::KEY,
+                "label" => "Id",
+                "showInList" => false,
+                "showInDetail" => false,
+                "nullable" => false],
+
+            "name" => [
+                "type" => DataType::STRING,
+                "label" => "Title",
+                "showInList" => true,
+                "showInDetail" => true,
+                "nullable" => false],
+        );
+    public $fk = array();
+    public $table = "privilege";
+
+    function __construct() { }
+}
+
+$items = [new User(), new Privilege()];
